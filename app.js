7b542e1b66cc7c1b32d04cf1ca11aab8a7a1ca6c
Steven <sky5998223@gmail.com>
2021-11-28 14:08:55 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const exphbs = require("express-handlebars")
+const Todo = require("./models/todo")
 
 const app = express()
 
@@ -23,7 +24,11 @@ app.set('view engine', 'hbs')
 
 
 app.get('/', (req, res) => {
-  res.render('index')
+  //取得所有Todo資料
+  Todo.find()
+    .lean()
+    .then(todos => res.render('index', { todos }))
+    .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {
