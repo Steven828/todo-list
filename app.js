2a30ab984c1934df1bc1dddae2f25cc75a190057
Steven <sky5998223@gmail.com>
2021-11-28 15:49:05 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ db.once('open', () => {
   console.log("mongodb connected!")
 })
 
+app.use(express.urlencoded({ extended: true }))
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
@@ -31,6 +32,21 @@ app.get('/', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name
+  return Todo.create({ name }) //請mongoose建立並傳入資料
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+  //另一種寫法，先在伺服器端建立實體再存入
+    //const todo = new Todo({ name })
+  //return todo.save() 
+    //.then(() => res.redirect('/'))
+    //.catch(error => console.log(error))
+})
 app.listen(3000, () => {
   console.log("App is running on port 3000")
 })
